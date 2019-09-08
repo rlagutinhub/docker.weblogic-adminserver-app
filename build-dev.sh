8d78e8f4e3545e5abf663c26469dc89c80b2cfb1
Roman Lagutin <30324030+rlagutinhub@users.noreply.github.com>
2019-09-08 11:46:53 +0300

    mod build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scriptDir="$( cd "$(dirname $0)" && pwd)"; cd $scriptDir
+scriptDir="$(cd "$(dirname $0)" && pwd)"; cd $scriptDir
 
 # 1.rm
 docker rm -f wls-app; docker image rm oracle/weblogic:12.2.1.2-generic_custom
@@ -9,8 +9,7 @@ docker rm -f wls-app; docker image rm oracle/weblogic:12.2.1.2-generic_custom
 ./build-app.sh
 
 # 3.image
-./build.sh "properties" "scripts" "files" "Dockerfile"  "oracle/weblogic:12.2.1.2-generic_custom" "/root/docker/wls-app"
+./build.sh "properties" "scripts" "files" "Dockerfile" "oracle/weblogic:12.2.1.2-generic_custom" "/root/docker/wls-app"
 
 # 4.container
 docker run -dit --name wls-app  --network bridge -p 7001:7001/tcp -p 9002:9002/tcp oracle/weblogic:12.2.1.2-generic_custom; docker logs --follow wls-app
-
