1673a19e7d02b3b659101583a1a13e25110c53fd
Roman Lagutin <30324030+rlagutinhub@users.noreply.github.com>
2019-09-09 11:43:46 +0300

    mod stop-timeout

--- build-dev.sh
+++ build-dev.sh
@@ -12,4 +12,4 @@ docker rm -f wls-app.$(date +%F); docker image rm oracle/weblogic:12.2.1.2-gener
 ./build.sh "properties" "scripts" "files" "Dockerfile" "oracle/weblogic:12.2.1.2-generic_custom.$(date +%F)" "."
 
 # 4.container
-docker run -dit --name wls-app.$(date +%F) --network bridge -p 7001:7001/tcp -p 9002:9002/tcp oracle/weblogic:12.2.1.2-generic_custom.$(date +%F); docker logs --follow wls-app.$(date +%F)
+docker run -dit --stop-timeout 120 --name wls-app.$(date +%F) --network bridge -p 7001:7001/tcp -p 9002:9002/tcp oracle/weblogic:12.2.1.2-generic_custom.$(date +%F); docker logs --follow wls-app.$(date +%F)
