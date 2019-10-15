822925c6a5d74672d75f976252d7b9e846881c65
Roman Lagutin <rlagutin@mta4.ru>
2019-10-15 09:41:37 +0300

    mod build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -3,14 +3,14 @@
 scriptDir="$(cd "$(dirname $0)" && pwd)"; cd $scriptDir
 
 # 1.rm
-docker rm -f wls-app.$(date +%F); docker image rm oracle/weblogic:12.2.1.4-generic_custom.$(date +%F)
+docker rm -f wls-app.$(date +%F); docker image rm oracle/weblogic:12.2.1.2-generic_custom.$(date +%F)
 
 # 2.app
 ./build-app.sh
 
 # 3.image
-./build.sh "properties" "scripts" "files" "Dockerfile" "oracle/weblogic:12.2.1.4-generic_custom.$(date +%F)" "."
+./build.sh "properties" "scripts" "files" "Dockerfile" "oracle/weblogic:12.2.1.2-generic_custom.$(date +%F)" "."
 
 # 4.container
-docker run -dit --stop-timeout 120 --name wls-app.$(date +%F) --network bridge -p 7001:7001/tcp -p 9002:9002/tcp oracle/weblogic:12.2.1.4-generic_custom.$(date +%F); docker logs --follow wls-app.$(date +%F)
+docker run -dit --stop-timeout 120 --name wls-app.$(date +%F) --network bridge -p 7001:7001/tcp -p 9002:9002/tcp oracle/weblogic:12.2.1.2-generic_custom.$(date +%F); docker logs --follow wls-app.$(date +%F)
 
